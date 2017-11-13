c7085fd7b939ea2ee06c5c26e40f900c0c8b509c
Jacob <jmooree30@gmail.com>
2017-11-12 20:48:12 -0500

    Added persistent sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:session][:email])
         if user && user.authenticate(params[:session][:password])
             log_in(user)
+            remember(user)
             flash[:success] = "You have been signed in!"
             redirect_to root_path
         else
